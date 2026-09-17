195aead69291b2439f2a37c0061e57cbd5339971
cguhher <cgunner@gmail.com>
2026-09-17 19:16:39 +0100

    Bump to v0.5 glance coach

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "uk.co.cgunner.meetingcoach"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4-conversation-timeline"
+        versionCode = 5
+        versionName = "0.5-glance-coach"
     }
 }
 
